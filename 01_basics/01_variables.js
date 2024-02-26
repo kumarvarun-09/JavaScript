b22aab6474c4866c8bcf7574ddd20835fa1f5088
Varun Kumar <97241400+kumarvarun-09@users.noreply.github.com>
2024-02-26 11:28:26 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 45454
+let email = "vk@gmail.com"
+var password = 123456
+city = "Kanpur"
+
+let state
+
+// accountId = 798754
+
+/*
+    Prefer not to use var because it creates problems with block scope and function scope
+*/
+
+email = 7798
+password = 4545
+city = 78787
+
+console.table([accountId, email, password, city, state])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // use all JS code as newer version of JavaScript
+
+// alert(3 + 3)  // this syntax is used in Browser not in Node and we are using Node.js
+
+let myName = "Varun"
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof myName)  // string 
+console.log(typeof null)    // object
+console.log(typeof undefined)   // undefined
